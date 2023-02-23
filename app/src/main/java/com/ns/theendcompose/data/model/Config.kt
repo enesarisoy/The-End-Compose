efeab765d93537850d2edd77b30157975444b12b
enesarisoy <arisoyeness@gmail.com>
2023-02-23 16:11:19 +0300

    images config

---
+++ app/src/main/java/com/ns/theendcompose/data/model/Config.kt
@@ -0,0 +1,12 @@
+package com.ns.theendcompose.data.model
+
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class Config(
+    @Json(name = "images")
+    val imagesConfig: ImagesConfig,
+    @Json(name = "change_keys")
+    val changeKeys: List<String>
+)
