0dc0a14199aa4999f4bbfdff7384bd055f25f998
enesarisoy <arisoyeness@gmail.com>
2023-02-23 15:17:04 +0300

    date json adapter

---
+++ app/src/main/java/com/ns/theendcompose/data/api/DateJsonAdapter.kt
@@ -0,0 +1,60 @@
+package com.ns.theendcompose.data.api
+
+import android.annotation.SuppressLint
+import com.squareup.moshi.FromJson
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.JsonReader
+import com.squareup.moshi.JsonWriter
+import com.squareup.moshi.ToJson
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.Date
+
+class DateJsonAdapter : JsonAdapter<Date>() {
+
+    private companion object {
+        @SuppressLint("SimpleDateFormat")
+        val serverDateFormat = SimpleDateFormat("yyyy-MM-dd")
+
+        @SuppressLint("SimpleDateFormat")
+        val isoDateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+    }
+
+    @FromJson
+    override fun fromJson(reader: JsonReader): Date? {
+        val dateString = reader.nextString()
+
+        if (dateString.isEmpty())
+            return null
+
+        val serverDateFormat = serverDateFormat.parseOrNull(dateString)
+        if (serverDateFormat != null) {
+            return serverDateFormat
+        }
+
+        val isoDate = isoDateFormat.parseOrNull(dateString)
+        if (isoDate != null) {
+            return isoDate
+        }
+
+        return null
+    }
+
+    @ToJson
+    override fun toJson(writer: JsonWriter, value: Date?) {
+        if (value != null) {
+            writer.value(value.toString())
+        }
+    }
+
+}
+
+private fun SimpleDateFormat.parseOrNull(source: String): Date? {
+    return try {
+        parse(source)
+    } catch (e: ParseException) {
+        null
+    } catch (e: ArrayIndexOutOfBoundsException) {
+        null
+    }
+}
