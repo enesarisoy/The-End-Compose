98385f60e7d7743f7ecba10c6cf8b5079336adeb
enesarisoy <arisoyeness@gmail.com>
2023-02-23 16:24:59 +0300

    device language

---
+++ app/src/main/java/com/ns/theendcompose/data/model/DeviceLanguage.kt
@@ -0,0 +1,14 @@
+package com.ns.theendcompose.data.model
+
+data class DeviceLanguage(
+    val region: String,
+    val languageCode: String
+){
+
+    companion object {
+        val default: DeviceLanguage = DeviceLanguage(
+            region = "US",
+            languageCode = "en-US"
+        )
+    }
+}
