f7d151201259fddb493e687dcb35b6b132525388
enesarisoy <arisoyeness@gmail.com>
2023-02-23 16:21:44 +0300

    date utils

--- app/src/main/java/com/ns/theendcompose/utils/DateUtils.kt
+++ app/src/main/java/com/ns/theendcompose/utils/DateUtils.kt
@@ -1,6 +1,10 @@
 package com.ns.theendcompose.utils
 
+import android.app.DatePickerDialog
+import android.content.Context
+import android.widget.DatePicker
 import java.text.SimpleDateFormat
+import java.util.Calendar
 import java.util.Date
 import java.util.Locale
 
@@ -10,3 +14,48 @@ fun Date.formatted(format: String = "dd.MM.yyyy"): String {
 }
 
 fun Date.yearString() = formatted(format = "yyyy")
+
+fun Date.timeString() = formatted(format = "HH:mm")
+
+fun yearRangeString(from: Date?, to: Date?): String {
+    return listOf(from, to)
+        .mapNotNull { date -> date?.yearString() }
+        .distinct()
+        .joinToString(separator = " - ")
+}
+
+fun createDateDialog(
+    context: Context,
+    initialDate: Date?,
+    minDate: Date? = null,
+    maxDate: Date? = null,
+    onDateSelected: (Date) -> Unit = {}
+): DatePickerDialog {
+    val (initYear, initMonth, initDayOfMonth) = Calendar.getInstance().apply {
+        time = initialDate ?: Date()
+    }.run {
+        Triple(get(Calendar.YEAR), get(Calendar.MONTH), get(Calendar.DAY_OF_MONTH))
+    }
+
+    return DatePickerDialog(
+        context,
+        { _: DatePicker, year: Int, month: Int, dayOfMonth: Int ->
+            val date = Calendar.getInstance().apply {
+                set(Calendar.YEAR, year)
+                set(Calendar.MONTH, month)
+                set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            }.time
+            onDateSelected(date)
+        }, initYear, initMonth, initDayOfMonth
+    ).apply {
+        val datePicker = datePicker
+
+        minDate?.let { date ->
+            datePicker.minDate = date.time
+        }
+
+        maxDate?.let { date ->
+            datePicker.maxDate = date.time
+        }
+    }
+}
