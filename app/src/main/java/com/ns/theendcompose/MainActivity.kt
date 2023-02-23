a57835d2b5ed9731ac4a7ca40e9029e5da848e96
enesarisoy <arisoyeness@gmail.com>
2023-02-23 15:09:08 +0300

    setup gradle

--- app/src/main/java/com/ns/theendcompose/MainActivity.kt
+++ app/src/main/java/com/ns/theendcompose/MainActivity.kt
@@ -8,14 +8,11 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.ns.theendcompose.ui.theme.TheEndComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TheEndComposeTheme {
-            }
         }
     }
 }
