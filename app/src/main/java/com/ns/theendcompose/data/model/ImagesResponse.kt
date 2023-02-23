e873e982d2df8987ebb6504b701b10f87d313835
enesarisoy <arisoyeness@gmail.com>
2023-02-23 16:32:06 +0300

    image

---
+++ app/src/main/java/com/ns/theendcompose/data/model/ImagesResponse.kt
@@ -0,0 +1,10 @@
+package com.ns.theendcompose.data.model
+
+import com.squareup.moshi.JsonClass
+
+@JsonClass(generateAdapter = true)
+data class ImagesResponse(
+    val id: Int,
+    val backdrops: List<Image>?,
+    val stills: List<Image>?
+)
