bf2def73efbeae4493c7a02c2ea3a71f104fe594
enesarisoy <arisoyeness@gmail.com>
2023-02-24 12:44:49 +0300

    mainviewmodel

--- app/src/main/java/com/ns/theendcompose/MainViewModel.kt
+++ app/src/main/java/com/ns/theendcompose/MainViewModel.kt
@@ -1,11 +1,48 @@
 package com.ns.theendcompose
 
+import androidx.lifecycle.viewModelScope
+import com.ns.theendcompose.data.model.SnackBarEvent
+import com.ns.theendcompose.data.repository.config.ConfigRepository
+import com.ns.theendcompose.utils.ImageUrlParser
+import com.ns.theendcompose.utils.NetworkStatus
 import com.ns.theendcompose.utils.NetworkStatusTracker
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val networkStatusTracker: NetworkStatusTracker
-) {
+    private val networkStatusTracker: NetworkStatusTracker,
+    private val configRepository: ConfigRepository
+): BaseViewModel() {
+
+    private val connectionStatus = networkStatusTracker.connectionStatus
+
+    val networkSnackBarEvent: StateFlow<SnackBarEvent?> = connectionStatus.mapLatest { status ->
+        when (status) {
+            NetworkStatus.Connected -> SnackBarEvent.NetworkConnected
+            NetworkStatus.Disconnected -> SnackBarEvent.NetworkDisconnected
+        }
+    }.stateIn(viewModelScope, SharingStarted.Eagerly, null)
+
+    private val sameBottomBarRouteChannel: Channel<String> = Channel()
+    val sameBottomRoute: Flow<String> =
+        sameBottomBarRouteChannel.receiveAsFlow().flowOn(Dispatchers.Main.immediate)
+
+    val imageUrlParser: StateFlow<ImageUrlParser?> = configRepository.getImageUrlParser()
+        .stateIn(viewModelScope, SharingStarted.Eagerly, null)
+
+    fun updateLocale() {
+        configRepository.updateLocale()
+    }
+
+    fun onSameRouteSelected(route: String) {
+        viewModelScope.launch(Dispatchers.Main.immediate){
+            sameBottomBarRouteChannel.send(route)
+        }
+
+    }
 }
