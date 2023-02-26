275920cdc8e9f66457447ffe2296555185daf028
enesarisoy <arisoyeness@gmail.com>
2023-02-26 15:27:05 +0300

    movie screen

--- app/src/main/java/com/ns/theendcompose/ui/screens/movie/MovieScreen.kt
+++ app/src/main/java/com/ns/theendcompose/ui/screens/movie/MovieScreen.kt
@@ -1,12 +1,23 @@
 package com.ns.theendcompose.ui.screens.movie
 
+import android.app.Activity
+import androidx.activity.compose.BackHandler
 import androidx.compose.animation.AnimatedVisibilityScope
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material3.AlertDialog
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.paging.compose.collectAsLazyPagingItems
 import com.ns.theendcompose.MainViewModel
+import com.ns.theendcompose.ui.screens.destinations.MovieScreenDestination
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import kotlinx.coroutines.flow.collectLatest
 
 @RootNavGraph(start = true)
 @Destination
@@ -16,5 +27,61 @@ fun AnimatedVisibilityScope.MovieScreen(
     viewModel: MovieScreenViewModel = hiltViewModel(),
     navigator: DestinationsNavigator
 ) {
+    val uiState by viewModel.uiState.collectAsState()
+    val scrollState = rememberScrollState()
 
+    LaunchedEffect(Unit) {
+        mainViewModel.sameBottomRoute.collectLatest { sameRoute ->
+            if (sameRoute == MovieScreenDestination.route) {
+                scrollState.animateScrollTo(0)
+            }
+        }
+    }
+
+    MoviesScreenContent(
+        uiState = uiState,
+        scrollState = scrollState
+    )
+}
+
+@Composable
+fun MoviesScreenContent(
+    uiState: MovieScreenUIState,
+    scrollState: ScrollState
+) {
+
+    val context = LocalContext.current
+    val density = LocalDensity.current
+
+    val upcomingLazyItems = uiState.moviesState.upcoming.collectAsLazyPagingItems()
+    val topRatedLazyItems = uiState.moviesState.topRated.collectAsLazyPagingItems()
+    val trendingLazyItems = uiState.moviesState.trending.collectAsLazyPagingItems()
+    val nowPlayingLazyItems = uiState.moviesState.nowPlaying.collectAsLazyPagingItems()
+    val favoritesLazyItems = uiState.favorites.collectAsLazyPagingItems()
+
+    var topSectionHeight: Float? by remember {
+        mutableStateOf(null)
+    }
+
+    val appBarHeight = density.run { 56.dp.toPx() }
+    val topSectionScrollLimitValue: Float? = topSectionHeight?.minus(appBarHeight)
+    var showExitDialog by remember {
+        mutableStateOf(false)
+    }
+    val dismissDialog = {
+        showExitDialog = false
+    }
+    BackHandler {
+        showExitDialog = true
+    }
+
+    if (showExitDialog) {
+        AlertDialog(
+            onDismissRequest = dismissDialog,
+            dismissButton = { dismissDialog },
+            confirmButton = {
+                val activity = (context as? Activity)
+                activity?.finish()
+            })
+    }
 }
