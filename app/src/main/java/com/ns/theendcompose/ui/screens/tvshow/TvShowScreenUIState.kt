ef19db9f306684780e52971bc60253612698816b
enesarisoy <arisoyeness@gmail.com>
2023-02-27 11:02:04 +0300

    tv screen

---
+++ app/src/main/java/com/ns/theendcompose/ui/screens/tvshow/TvShowScreenUIState.kt
@@ -0,0 +1,41 @@
+package com.ns.theendcompose.ui.screens.tvshow
+
+import androidx.paging.PagingData
+import com.ns.theendcompose.data.model.DetailPresentable
+import com.ns.theendcompose.data.model.Presentable
+import com.ns.theendcompose.data.model.tvshow.RecentlyBrowsedTvShow
+import com.ns.theendcompose.data.model.tvshow.TvShowFavorite
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.emptyFlow
+
+data class TvShowScreenUIState(
+    val tvShowsState: TvShowsState,
+    val favorites: Flow<PagingData<TvShowFavorite>>,
+    val recentlyBrowsed: Flow<PagingData<RecentlyBrowsedTvShow>>
+) {
+    companion object {
+        val default: TvShowScreenUIState = TvShowScreenUIState(
+            tvShowsState = TvShowsState.default,
+            favorites = emptyFlow(),
+            recentlyBrowsed = emptyFlow()
+        )
+    }
+}
+
+data class TvShowsState(
+    val onTheAir: Flow<PagingData<DetailPresentable>>,
+    val discover: Flow<PagingData<Presentable>>,
+    val topRated: Flow<PagingData<Presentable>>,
+    val trending: Flow<PagingData<Presentable>>,
+    val airingToday: Flow<PagingData<Presentable>>,
+) {
+    companion object {
+        val default: TvShowsState = TvShowsState(
+            onTheAir = emptyFlow(),
+            discover = emptyFlow(),
+            topRated = emptyFlow(),
+            trending = emptyFlow(),
+            airingToday = emptyFlow(),
+        )
+    }
+}
